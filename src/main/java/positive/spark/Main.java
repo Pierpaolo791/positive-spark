21f00d3d2001bf1bcc0ac9cab9d9efb6e29e234d
Pierpaolo Pecoraio <pierpaolo.pecoraio@gmail.com>
2020-10-14 17:10:12 +0200

    prova esempio prof

--- src/main/java/positive/spark/Main.java
+++ src/main/java/positive/spark/Main.java
@@ -1,27 +1,8 @@
 package positive.spark;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Set;
-import java.util.regex.Pattern;
-
-import scala.Tuple2;
-
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.common.serialization.StringDeserializer;
-
-import org.apache.spark.SparkConf;
-import org.apache.spark.streaming.api.java.*;
-import org.apache.spark.streaming.kafka010.ConsumerStrategies;
-import org.apache.spark.streaming.kafka010.KafkaUtils;
-import org.apache.spark.streaming.kafka010.LocationStrategies;
-import org.apache.spark.streaming.Durations;
 public class Main {
 	
-		PositiveSpark positiveSpark = new PositiveSpark(); 
-		
+	    public static void main(String[] args) throws Exception {
+	    	PositiveSpark positiveSpark = new PositiveSpark(); 
 	}
 
 }
