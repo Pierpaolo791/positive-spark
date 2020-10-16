bef708b110d9a39382dd873b51a0d9178bb9bc38
Pierpaolo Pecoraio <pierpaolo.pecoraio@gmail.com>
2020-10-16 22:56:43 +0200

    v0

--- src/main/java/positive/spark/Main.java
+++ src/main/java/positive/spark/Main.java
@@ -1,14 +1,9 @@
 package positive.spark;
 
-import com.vader.sentiment.analyzer.SentimentAnalyzer;
-
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-		//new PositiveSpark();	
-		SentimentAnalyzer sentiment = new SentimentAnalyzer("Good day");
-		sentiment.analyze();
-		System.out.println(sentiment.getPolarity());
+		new PositiveSpark();	
 	}
 
 }
